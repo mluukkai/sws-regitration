3bfe0cffbbc9a721d1fa505999e91025d21c4512
Matti Luukkainen <mluukkai@iki.fi>
2016-12-11 12:20:12 +0200

    trollbar

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module SwsReg
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.enabled = true
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_dispatch.default_headers.merge!('Cache-Control' => 'no-store, no-cache')
   end
 end
